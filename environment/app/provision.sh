4d83d4667477ad2abc38ce4bae983f27367bdfae
Kevin Monteiro <kevin.monteiro96@gmail.com>
2020-04-08 18:38:42 +0100

    updated provisions

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -1,4 +1,6 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo pip3 install -r /home/ubuntu/app/requirements.txt
+sudo apt-get install python3-pip -y
+sudo pip3 install -r /home/vagrant/app/requirements.txt
+sudo chmod 777 /home/vagrant/Downloads/
